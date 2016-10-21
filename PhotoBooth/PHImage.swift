432a3527b7b618922d850c6a86f8f139c03073ee
wasupol <wasupol@unicornstudio.net>
2016-10-21 16:47:09 +0700

    add new object for watermark

---
+++ PhotoBooth/PHImage.swift
@@ -0,0 +1,9 @@
+//
+//  PHImage.swift
+//  PhotoBooth
+//
+//  Created by Wasupol Tungsakultong on 10/21/2559 BE.
+//  Copyright © 2559 Wasupol Tungsakultong. All rights reserved.
+//
+
+import Foundation
